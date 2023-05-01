080c9647f0d04fa0f754f66908a80668e19819ab
CodeBase <basecodeacc@gmail.com>
2023-05-01 20:07:44 +0530

    Chnaged emojis to SVG Icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+
+import React, { useState, useEffect, useRef } from 'react';
+// import { CSSTransition } from 'react-transition-group';
 function App() {
     return (
         <Navbar>    
-            <NavItem icon="😃 "/>
-            <NavItem icon="😃 "/>
-            <NavItem icon="😃 "/>
+            <NavItem icon={<PlusIcon/>}/>
+            <NavItem icon={<BellIcon/>}/>
+            <NavItem icon={<MessengerIcon/>}/>
         </Navbar>
     );
 }
