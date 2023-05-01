5f38ab2432b894b65b8a97fb5f01c1bc791658c1
CodeBase <basecodeacc@gmail.com>
2023-05-01 18:01:24 +0530

    Added HTML and CSS for navbar buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
 function App() {
     return (
-        <Navbar/>
+        <Navbar>    
+            <NavItem icon="😃 "/>
+            <NavItem icon="😃 "/>
+            <NavItem icon="😃 "/>
+        </Navbar>
     );
 }
-function Navbar() {
+function Navbar(props) {
     return (
         <nav className="navbar">
             <ul className="navbar-nav">
-
+                {props.children}
             </ul>
         </nav>
+    )
+}
+function NavItem(props) {
+    return (
+        <li className="nav-item">
+            <a href="#" className="icon-button">
+                {props.icon}
+            </a>
+        </li>
     )
 }
 export default App;
